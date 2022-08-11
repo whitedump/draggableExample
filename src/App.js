92093d08ac8b3360da53771ba781d3af988d653c
Anton Miseika <anton.miseika@gmail.com>
2022-08-11 13:32:42 +0300

    bla2

--- src/App.js
+++ src/App.js
@@ -5,33 +5,6 @@ import {ResizableBox} from "react-resizable";
 import "react-resizable/css/styles.css";
 import AutoScale from './AutoScale'
 
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
-  });
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
-}
-
 export default function App() {
   const [disableDrag, setDisableDrag] = useState(false)
   const [defaultPos, setDefaultPos] = useState({
@@ -44,7 +17,6 @@ export default function App() {
   })
   const [widgetWidth, setWidgetWidth] = useState(1000);
   const [widgetHeight, setWidgetHeight] = useState(800);
-  const {width: screenWidth, height: screenHeight} = useWindowSize();
 
   var lastX = window.innerWidth
   var lastY = window.innerHeight
